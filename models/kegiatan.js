b2272eb38c05f29033f4ae8035748a1090eb47b2
Dustin Bayu Herlambang <114048736+Zerexxn@users.noreply.github.com>
2022-12-02 20:45:25 +0700

    Update kegiatan.js

--- models/kegiatan.js
+++ models/kegiatan.js
@@ -12,7 +12,6 @@ const kegiatanSchema = new mongoose.Schema({
        type: String,
        trim: true,
        required : [true, 'Please add a product Name'],
-       maxlength: 100
    },
 
    tgl_kegiatan: {
